27666c1db3193c9e733ee1d7414c369471e83cf7
Tanner Widis <tewidis@gmail.com>
2019-08-25 11:56:56 -0500

    Wrote script to download csv data for all teams in the AUDL. Also version controlling the data since it's small.

---
+++ data/download_data.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Atlanta Hustle
+curl -LO https://www.ultianalytics.com/rest/view/team/4597087672991744/stats/export
+mv export atlanta_hustle.csv
+
+# Austin Sol
+curl -LO https://www.ultianalytics.com/rest/view/team/6637781254144000/stats/export
+mv export austin_sol.csv
+
+# Chicago Wildfire
+curl -LO https://www.ultianalytics.com/rest/view/team/5684092356001792/stats/export
+mv export chicago_wildfire.csv
+
+# DC Breeze
+curl -LO https://www.ultianalytics.com/rest/view/team/4848353275805696/stats/export
+mv export dc_breeze.csv
+
+# Dallas Roughnecks
+curl -LO https://www.ultianalytics.com/rest/view/team/5501394781995008/stats/export
+mv export dallas_roughnecks.csv
+
+# Detroit Mechanix
+curl -LO https://www.ultianalytics.com/rest/view/team/6466345621258240/stats/export
+mv export detroit_mechanix.csv
+
+# Indianapolis AlleyCats
+curl -LO https://www.ultianalytics.com/rest/view/team/6099543552688128/stats/export
+mv export indianapolis_alleycats.csv
+
+# Los Angeles Aviators
+curl -LO https://www.ultianalytics.com/rest/view/team/6417832975794176/stats/export
+mv export los_angeles_aviators.csv
+
+# Madison Radicals
+curl -LO https://www.ultianalytics.com/rest/view/team/5335870970265600/stats/export
+mv export madison_radicals.csv
+
+# Minnesota Wind Chill
+curl -LO https://www.ultianalytics.com/rest/view/team/5223179500912640/stats/export
+mv export minnesota_wind_chill.csv
+
+# Montreal Royal
+curl -LO https://www.ultianalytics.com/rest/view/team/5798954679664640/stats/export
+mv export montreal_royal.csv
+
+# New York Empire
+curl -LO https://www.ultianalytics.com/rest/view/team/6080525840154624/stats/export
+mv export new_york_empire.csv
+
+# Ottawa Outlaws
+curl -LO https://www.ultianalytics.com/rest/view/team/5340445714415616/stats/export
+mv export ottawa_outlaws.csv
+
+# Philadelphia Phoenix
+curl -LO https://www.ultianalytics.com/rest/view/team/6369964004999168/stats/export
+mv export philadelphia_phoenix.csv
+
+# Pittsburgh Thunderbirds
+curl -LO https://www.ultianalytics.com/rest/view/team/5131989745139712/stats/export
+mv export pittsburgh_thunderbirds.csv
+
+# Raleigh Flyers
+curl -LO https://www.ultianalytics.com/rest/view/team/5647500610371584/stats/export
+mv export raleigh_flyers.csv
+
+# San Diego Growlers
+curl -LO https://www.ultianalytics.com/rest/view/team/6026025574072320/stats/export
+mv export san_diego_growlers.csv
+
+# San Jose Spiders
+curl -LO https://www.ultianalytics.com/rest/view/team/5661186758541312/stats/export
+mv export san_jose_spiders.csv
+
+# Seattle Cascades
+curl -LO https://www.ultianalytics.com/rest/view/team/5405478582484992/stats/export
+mv export seattle_cascades.csv
+
+# Tampa Bay Cannons
+curl -LO https://www.ultianalytics.com/rest/view/team/5775526035718144/stats/export
+mv export tampa_bay_cannons.csv
+
+# Toronto Rush
+curl -LO https://www.ultianalytics.com/rest/view/team/5660854234120192/stats/export
+mv export toronto_rush.csv
+
